d0bd0903e4f98b128704b56ad2087e322149afcb
rafaelchaves <rafaelvchaves@gmail.com>
2019-11-01 21:29:57 -0400

    Bug fixes

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,10 @@ cp *.html ./client
 cp -R ./images ./client
 cp user_types.json ./client
 cp lunches.json ./client
-<<<<<<< HEAD
 cp 2019-20_calendar.ics ./client
 cp blacklist ./client
 cp whitelist ./client
-=======
 cp letter_days.ics ./client
 cp blacklist ./client
->>>>>>> bea4bfda03b01ecc8817e8ff7e2e78e5288807ea
 serverless client deploy
 rm -rf client
